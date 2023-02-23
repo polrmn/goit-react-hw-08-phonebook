d2bb1c65b28b3cb300377adf5551a7bbf3cd1931
polrmn <polrmn@gmail.com>
2023-02-23 19:56:28 +0200

    delete outlet from public route

--- src/components/PublicRoute/PublicRoute.jsx
+++ src/components/PublicRoute/PublicRoute.jsx
@@ -1,5 +1,5 @@
 import { useSelector } from "react-redux";
-import { Navigate, Outlet } from "react-router-dom";
+import { Navigate } from "react-router-dom";
 import { getToken } from "redux/selectors";
 
 const PublicRoute = ({component}) => {
